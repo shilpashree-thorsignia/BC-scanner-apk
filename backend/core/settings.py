6081f5b9bc002ee0f6d5732082c02c99a2dfc7c6
shilpa <shilpa@gmail.com>
2025-06-02 17:46:46 +0530

    database switch sqlite to mysql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,10 +54,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# MySQL configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bc_scanner_db',
+        'USER': 'root',  
+        'PASSWORD': 'MySQL',  
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
